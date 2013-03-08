1d4c530678d4395da40c4e232e6adcc318362e8e
Bajee11 <bajee11@gmail.com>
2013-03-07 21:25:27 -0500

    MMS Breath
    Credits: @Kroz for the icons and idea!
    
    This will give your SMS notifications a breathing effect in the statusbar instead of an static icon.
    
    Reworked by mnazim for super simplified implementation
    
    Change-Id: I50f0e694151ed1237e838aa61c1e43f8d7fb10c0
    
    Conflicts:
    	res/xml/preferences.xml
    	src/com/android/mms/ui/MessagingPreferenceActivity.java

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -71,6 +71,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String RETRIEVAL_DURING_ROAMING = "pref_key_mms_retrieval_during_roaming";
     public static final String AUTO_DELETE              = "pref_key_auto_delete";
     public static final String GROUP_MMS_MODE           = "pref_key_mms_group_mms";
+    public static final String MMS_BREATH               = "mms_breath";
 
     // Unicode
     public static final String UNICODE_STRIPPING            = "pref_key_unicode_stripping";
@@ -153,6 +154,7 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     private CheckBoxPreference mEnableQmLockscreenPref;
     private CheckBoxPreference mEnableQmCloseAllPref;
     private CheckBoxPreference mEnableQmDarkThemePref;
+    private CheckBoxPreference mMMSBreath;
 
     @Override
     protected void onCreate(Bundle icicle) {
@@ -229,6 +231,8 @@ public class MessagingPreferenceActivity extends PreferenceActivity
         mGestureSensitivity = (ListPreference) findPreference(GESTURE_SENSITIVITY);
         mUnicodeStripping = (ListPreference) findPreference(UNICODE_STRIPPING);
         mUnicodeStrippingEntries = getResources().getTextArray(R.array.pref_unicode_stripping_entries);
+        mMMSBreath = (CheckBoxPreference) findPreference(MMS_BREATH);
+        mMMSBreath.setChecked(mMMSBreath.isChecked());
 
         // QuickMessage
         mEnableQuickMessagePref = (CheckBoxPreference) findPreference(QUICKMESSAGE_ENABLED);
@@ -505,6 +509,9 @@ public class MessagingPreferenceActivity extends PreferenceActivity
 
             // Update "enable dark theme" checkbox state
             mEnableQmDarkThemePref.setEnabled(!mEnablePrivacyModePref.isChecked());
+
+        } else if (preference == mMMSBreath) {
+            mMMSBreath.setChecked(mMMSBreath.isChecked());
 
         } else if (preference == mEnableQuickMessagePref) {
             // Update the actual "enable quickmessage" value that is stored in secure settings.
