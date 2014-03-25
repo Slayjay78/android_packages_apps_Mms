8413fbb1824a0d7f1df55be1466abde97d8fe574
yanglv <yanglv@codeaurora.org>
2014-03-25 18:16:07 +0530

    Mms: Draft still exists after forwarding mms
    The draft still exists after forwarding mms to the same
    sender
    
    Forward message will be saved as draft before sent, and it will reload
    this draft after come back to the current thread.
    
    Do not load the draft for the same recipient address when forwarding
    mms.
    
    Change-Id: I2b21fb527f00959ae9608675226b72d3ce85cb56

--- src/com/android/mms/data/Conversation.java
+++ src/com/android/mms/data/Conversation.java
@@ -104,6 +104,8 @@ public class Conversation {
     private static Object sDeletingThreadsLock = new Object();
     private boolean mMarkAsReadBlocked;
     private boolean mMarkAsReadWaiting;
+    private boolean mHasMmsForward = false; // True if has forward mms
+    private String mForwardRecipientNumber; // The recipient that the forwarded Mms received from
 
     private Conversation(Context context) {
         mContext = context;
@@ -1569,5 +1571,21 @@ public class Conversation {
                     " recipient from DB: " + address);
         }
         return address;
+    }
+
+    public boolean getHasMmsForward() {
+        return mHasMmsForward;
+    }
+
+    public void setHasMmsForward(boolean value) {
+        mHasMmsForward = value;
+    }
+
+    public String getForwardRecipientNumber() {
+        return mForwardRecipientNumber;
+    }
+
+    public void setForwardRecipientNumber(String forwardRecipientNumber) {
+        mForwardRecipientNumber = forwardRecipientNumber;
     }
 }
